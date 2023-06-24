202e36318be0ca0e451742a84535568d1e9e57a9
stefan-b10 <stefan_budai88@yahoo.com>
2023-06-24 19:26:31 +0300

    modified:   frontend/App.js modified:   frontend/assets/global.css new file:   frontend/assets/metamark.png modified:   frontend/ui-components.js

--- frontend/App.js
+++ frontend/App.js
@@ -4,46 +4,41 @@ import QRCode from "react-qr-code";
 
 import "./assets/global.css";
 
-import {
-	Header,
-	SignInPrompt,
-	SignOutButton,
-	MyWalletButton,
-	ReferencesButton,
-	NotificationsButton,
-	FindJobsButton,
-	DeployContract,
-} from "./ui-components";
+import { Header, SignInPrompt, SignOutButton, MyWalletButton, ReferencesButton, NotificationsButton, FindJobsButton, DeployContract } from "./ui-components";
 
 export default function App({ isSignedIn, wallet, isDeployed }) {
-	// const [valueFromBlockchain, setValueFromBlockchain] = React.useState();
+  // const [valueFromBlockchain, setValueFromBlockchain] = React.useState();
 
-	const [firstName, setFirstName] = React.useState();
-	const [lastName, setLastName] = React.useState();
-	const [email, setEmail] = React.useState();
-	const [phone, setPhone] = React.useState();
+  const [firstName, setFirstName] = React.useState();
+  const [lastName, setLastName] = React.useState();
+  const [email, setEmail] = React.useState();
+  const [phone, setPhone] = React.useState();
+  const [experience, setExperience] = React.useState({ position: "", company: "", location: "", description: "", startYear: "", endYear: "" });
+  const [education, setEducation] = React.useState({ school: "", degree: "", fieldOfStudy: "", startYear: "", endYear: "" });
 
-	const [uiPleaseWait, setUiPleaseWait] = React.useState(true);
+  const [uiPleaseWait, setUiPleaseWait] = React.useState(true);
 
-	// Get blockchian state once on component load
-	React.useEffect(() => {
-		if (isDeployed) {
-			getFirstName().then(setFirstName).catch(alert);
-			getLastName().then(setLastName).catch(alert);
-			getEmail().then(setEmail).catch(alert);
-			getPhone().then(setPhone).catch(alert);
-		}
+  // Get blockchian state once on component load
+  React.useEffect(() => {
+    if (isDeployed) {
+      getFirstName().then(setFirstName).catch(alert);
+      getLastName().then(setLastName).catch(alert);
+      getEmail().then(setEmail).catch(alert);
+      getPhone().then(setPhone).catch(alert);
+      getExperience().then(setExperience).catch(alert);
+      getEducation().then(setEducation).catch(alert);
+    }
 
-		// .finally(() => setUiPleaseWait(false));
-		// getGreeting()
-		//   .then(setValueFromBlockchain)
-		//   .catch(alert)
-		//   .finally(() => {
-		//     setUiPleaseWait(false);
-		//   });
-	}, []);
+    // .finally(() => setUiPleaseWait(false));
+    // getGreeting()
+    //   .then(setValueFromBlockchain)
+    //   .catch(alert)
+    //   .finally(() => {
+    //     setUiPleaseWait(false);
+    //   });
+  }, []);
 
-	/*
+  /*
 	/// If user not signed-in with wallet - show prompt
 	if (!isSignedIn) {
 		// Sign-in flow will reload the page later
@@ -56,7 +51,7 @@ export default function App({ isSignedIn, wallet, isDeployed }) {
 	}
   */
 
-	/*
+  /*
   function changeGreeting(e) {
     e.preventDefault();
     setUiPleaseWait(true);
@@ -77,24 +72,22 @@ export default function App({ isSignedIn, wallet, isDeployed }) {
   }
   */
 
-	// function deployContract(e) {
-	// 	e.preventDefault();
-	// 	console.log(e);
-	// }
+  // function deployContract(e) {
+  // 	e.preventDefault();
+  // 	console.log(e);
+  // }
 
-	const deployContract = async (e) => {
-		e.preventDefault();
-		const firstName = e.target.elements.firstname.value;
-		const lastName = e.target.elements.lastname.value;
-		if (firstName != "" && lastName != "") {
-			// console.log(wallet.wallet);
+  const deployContract = async (e) => {
+    e.preventDefault();
+    const firstName = e.target.elements.firstname.value;
+    const lastName = e.target.elements.lastname.value;
+    if (firstName != "" && lastName != "") {
+      const wasm = await fetch("./assets/resume.wasm");
 
-			const wasm = await fetch("./assets/resume.wasm");
+      const result = wallet.deploy(wasm);
+      console.log(result);
 
-			const result = wallet.deploy(wasm);
-			console.log(result);
-
-			/*
+      /*
 			fetch("./assets/resume.wasm").then((res) => {
 				console.log(res);
 				console.log(res.body);
@@ -106,142 +99,197 @@ export default function App({ isSignedIn, wallet, isDeployed }) {
 				});
 			});
 			*/
-		}
-	};
+    }
+  };
 
-	function getFirstName() {
-		return wallet.viewMethod(wallet.accountId, "get_first_name");
-	}
+  function getFirstName() {
+    return wallet.viewMethod(wallet.accountId, "get_first_name");
+  }
 
-	function getLastName() {
-		return wallet.viewMethod(wallet.accountId, "get_last_name");
-	}
+  function getLastName() {
+    return wallet.viewMethod(wallet.accountId, "get_last_name");
+  }
 
-	function getEmail() {
-		return wallet.viewMethod(wallet.accountId, "get_email");
-	}
+  function getEmail() {
+    return wallet.viewMethod(wallet.accountId, "get_email");
+  }
 
-	function getPhone() {
-		return wallet.viewMethod(wallet.accountId, "get_phone_number");
-	}
+  function getPhone() {
+    return wallet.viewMethod(wallet.accountId, "get_phone_number");
+  }
 
-	return (
-		<>
-			<header
-				style={{
-					display: "flex",
-					justifyContent: "space-between",
-					padding: "10px",
-				}}
-			>
-				<Header />
-				<div>
-					{isSignedIn ? (
-						<SignOutButton
-							accountId={wallet.accountId}
-							onClick={() => wallet.signOut()}
-						/>
-					) : (
-						<SignInPrompt onClick={() => wallet.signIn()} />
-					)}
-				</div>
-			</header>
+  function getExperience() {
+    return wallet.viewMethod(wallet.accountId, "get_experience");
+  }
 
-			<div className="mainbox">
-				<div className="row">
-					<div className="column side">
-						<div className="name container">
-							<QRCode
-								className="qr"
-								value={isDeployed ? wallet.accountId : "Contract not deployed"}
-							></QRCode>
-							<h1 className="name full">
-								{firstName} {lastName}
-							</h1>
-						</div>
+  function getEducation() {
+    return wallet.viewMethod(wallet.accountId, "get_education");
+  }
 
-						<MyWalletButton />
-						<ReferencesButton />
-						<NotificationsButton />
-						<FindJobsButton />
-					</div>
+  function getMonthAndYearFromEpochTime(epochTimeInSeconds) {
+    const date = new Date(epochTimeInSeconds * 1000);
+    const month = date.toLocaleString("default", { month: "short" });
+    const year = date.getFullYear();
 
-					<div className="column middle">
-						<div className="middlebox">
-							{isDeployed ? (
-								<div>
-									<h1>CV</h1>
-									<div className="resume">
-										<div className="fullname">
-											<span className="first name">{firstName} </span>
-											<span className="last name">{lastName}</span>
-										</div>
-										<div className="contact info">
-											<span className="email">Email: </span>
-											<span className="email val">{email}</span>
-											<span className="separator"></span>
-											<span className="email">Phone: </span>
-											<span className="email val">{phone}</span>
-										</div>
-										<div className="section">
-											<div className="section_title" >Experience</div>
-										</div>
-									</div>
-								</div>
-							) : (
-								<>
-									<h1>Welcome to your Blockchain Resume</h1>
-									<p>
-										You don`t have a Resume contract deployed on blockchain yet.
-										Please enter your First name, Last name and deploy your
-										resume contract.
-									</p>
+    return { month, year };
+  }
 
-									<form
-										style={{
-											display: "flex",
-											flexDirection: "column",
-											justifyContent: "center",
-											alignItems: "center",
-											marginTop: "2rem",
-										}}
-										onSubmit={deployContract}
-									>
-										<label className="deploy label" for="firstname">
-											First name:
-										</label>
-										<input
-											className="deploy input"
-											type="text"
-											id="firstname"
-										/>
-										<label className="deploy label" for="lasttname">
-											Last name:
-										</label>
-										<input
-											className="deploy input"
-											type="text"
-											id="lastname"
-										></input>
+  return (
+    <>
+      <header
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          padding: "10px",
+        }}
+      >
+        <Header />
+        <div>{isSignedIn ? <SignOutButton accountId={wallet.accountId} onClick={() => wallet.signOut()} /> : <SignInPrompt onClick={() => wallet.signIn()} />}</div>
+      </header>
 
-										<div style={{ marginTop: "1rem" }}>
-											<DeployContract />
-										</div>
-									</form>
-								</>
-							)}
-						</div>
-					</div>
+      <div className="mainbox">
+        <div className="row">
+          <div className="column side">
+            <div className="name container">
+              <QRCode className="qr" value={isDeployed ? wallet.accountId : "Contract not deployed"}></QRCode>
+              <h1 className="name full">
+                {firstName} {lastName}
+              </h1>
+            </div>
 
-					<div className="column side">
-						<h1>asdasdsad</h1>
-						<h1>asdasdsad</h1>
-						<h1>asdasdsad</h1>
-						<h1>asdasdsad</h1>
-						<h1>asdasdsad</h1>
-					</div>
-				</div>
-			</div>
-		</>
-	);
+            <MyWalletButton />
+            <ReferencesButton />
+            <NotificationsButton />
+            <FindJobsButton />
+          </div>
+
+          <div className="column middle">
+            <div className="middlebox">
+              {isDeployed ? (
+                <div>
+                  <h1>CV</h1>
+                  <div className="resume">
+                    <div className="fullname">
+                      <span className="first name">{firstName} </span>
+                      <span className="last name">{lastName}</span>
+                    </div>
+                    <div className="contact info">
+                      <span className="email">Email: </span>
+                      <span className="email val">{email}</span>
+                      <span className="separator"></span>
+                      <span className="email">Phone: </span>
+                      <span className="email val">{phone}</span>
+                    </div>
+                    <div className="section">
+                      <div className="section_title">Experience</div>
+                      <div className="section_list">
+                        <div>
+                          {experience.length > 0 ? (
+                            experience.map((el) => (
+                              <div className="section_list_item">
+                                <div className="left">
+                                  <div className="list_bold">{el.company}</div>
+                                  <div className="list_text">{el.location}</div>
+                                  <div className="list_text">
+                                    {getMonthAndYearFromEpochTime(el.startYear).month} {getMonthAndYearFromEpochTime(el.startYear).year} - {getMonthAndYearFromEpochTime(el.endYear).month}{" "}
+                                    {getMonthAndYearFromEpochTime(el.endYear).year}
+                                  </div>
+                                </div>
+                                <div className="right">
+                                  <div className="list_bold">{el.position}</div>
+                                  <div className="list_text">{el.description}sd d fdsfsdfdsf ds fsdfdsfdsfdsf dsfsdfdsfds fdsf dsfdsfdsf</div>
+                                </div>
+                              </div>
+                            ))
+                          ) : (
+                            <></>
+                          )}
+                        </div>
+                      </div>
+                    </div>
+                    <div className="section">
+                      <div className="section_title">Education</div>
+                      <div className="section_list">
+                        <div>
+                          {education.length > 0 ? (
+                            education.map((el) => (
+                              <div className="section_list_item">
+                                <div className="left">
+                                  <div className="list_bold">{el.school}</div>
+                                  <div className="list_text">
+                                    {getMonthAndYearFromEpochTime(el.startYear).month} {getMonthAndYearFromEpochTime(el.startYear).year} - {getMonthAndYearFromEpochTime(el.endYear).month}{" "}
+                                    {getMonthAndYearFromEpochTime(el.endYear).year}
+                                  </div>
+                                </div>
+                                <div className="right">
+                                  <div className="list_bold">{el.degree}</div>
+                                  <div className="list_text">{el.fieldOfStudy}</div>
+                                </div>
+                              </div>
+                            ))
+                          ) : (
+                            <></>
+                          )}
+                        </div>
+                        {/* <div>
+                          {education.length > 0 ? (
+                            education.map((el) => {
+                              <div className="section_list_item">
+                                <div className="left">
+                                  <div className="list_bold">{el.school}</div>
+                                </div>
+                              </div>;
+                            })
+                          ) : (
+                            <></>
+                          )}
+                        </div> */}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ) : (
+                <>
+                  <h1>Welcome to your Blockchain Resume</h1>
+                  <p>You don`t have a Resume contract deployed on blockchain yet. Please enter your First name, Last name and deploy your resume contract.</p>
+
+                  <form
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      marginTop: "2rem",
+                    }}
+                    onSubmit={deployContract}
+                  >
+                    <label className="deploy label" for="firstname">
+                      First name:
+                    </label>
+                    <input className="deploy input" type="text" id="firstname" />
+                    <label className="deploy label" for="lasttname">
+                      Last name:
+                    </label>
+                    <input className="deploy input" type="text" id="lastname"></input>
+
+                    <div style={{ marginTop: "1rem" }}>
+                      <DeployContract />
+                    </div>
+                  </form>
+                </>
+              )}
+            </div>
+          </div>
+
+          <div className="column side">
+            <h1>asdasdsad</h1>
+            <h1>asdasdsad</h1>
+            <h1>asdasdsad</h1>
+            <h1>asdasdsad</h1>
+            <h1>asdasdsad</h1>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- frontend/ui-components.js
+++ frontend/ui-components.js
@@ -1,80 +1,77 @@
 import React from "react";
 import logo from "./assets/logo.png";
+import mark from "./assets/metamark.png";
 import wallet from "./assets/wallet.png";
 import checkbox from "./assets/checked-checkbox.png";
-import notifications from "./assets/notifications.png"
-import find from "./assets/find.png"
+import notifications from "./assets/notifications.png";
+import find from "./assets/find.png";
 
 export function Header() {
-	return (
-		<header>
-			<div className="logo">
-				<img
-					src={logo}
-					alt="Logo"
-					style={{ width: "150px", height: "100px" }}
-				/>
-			</div>
-		</header>
-	);
+  return (
+    <header>
+      <div className="logo">
+        <img src={mark} alt="Logo" style={{ width: "90px", height: "75px" }} />
+      </div>
+    </header>
+  );
 }
 
 export function SignInPrompt({ onClick }) {
-	return (
-		<button style={{ float: "right" }} onClick={onClick}>
-			Sign in with NEAR Wallet
-		</button>
-	);
+  return (
+    <button style={{ float: "right" }} onClick={onClick}>
+      Sign in with NEAR Wallet
+    </button>
+  );
 }
 
 export function SignOutButton({ accountId, onClick }) {
-	return (
-		<button style={{ float: "right" }} onClick={onClick}>
-			Sign out {accountId}
-		</button>
-	);
+  return (
+    <button style={{ float: "right" }} onClick={onClick}>
+      Sign out {accountId}
+    </button>
+  );
 }
 
 export function MyWalletButton() {
-	return (
-		<button className="left buttons">
-			<img src={wallet}></img>
-			<span style={{ marginLeft: "25px" }}>My Wallet</span>
-		</button>
-	);
+  return (
+    <button className="left_buttons">
+      <img src={wallet}></img>
+      <span style={{ marginLeft: "25px" }}>My Wallet</span>
+    </button>
+  );
 }
 
 export function ReferencesButton() {
-	return (
-		<button className="left buttons">
-			<img src={checkbox}></img>
-			<span style={{ marginLeft: "25px" }}>References</span>
-		</button>
-	);
+  return (
+    <button className="left_buttons">
+      <img src={checkbox}></img>
+      <span style={{ marginLeft: "25px" }}>References</span>
+    </button>
+  );
 }
 
 export function NotificationsButton() {
-	return (
-		<button className="left buttons">
-			<img src={notifications}></img>
-			<span style={{ marginLeft: "25px" }}>Notifications</span>
-		</button>
-	);
+  return (
+    <button className="left_buttons">
+      <img src={notifications}></img>
+      <span style={{ marginLeft: "25px" }}>Notifications</span>
+    </button>
+  );
 }
 
 export function FindJobsButton() {
-	return (
-		<button className="left buttons">
-			<img src={find}></img>
-			<span style={{ marginLeft: "25px" }}>Find Jobs</span>
-		</button>
-	);
+  return (
+    <button className="left_buttons">
+      <img src={find}></img>
+      <span style={{ marginLeft: "25px" }}>Find Jobs</span>
+    </button>
+  );
 }
 
 export function DeployContract({ onClick }) {
-	return (
-		<button type="submit" onClick={onClick}>
-			Deploy Resume Contract
-		</button>
-	);
+  return (
+    <button type="submit" onClick={onClick}>
+      Deploy Resume Contract
+    </button>
+  );
 }
